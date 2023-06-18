17873a94bcaca2e31dfc2ed46451e89273b0d8d8
jgabrielgalvao <josegabriel755@gmail.com>
2023-06-18 17:51:55 -0300

    18_06

--- src/main/java/com/example/Trab_TPI/controller/FigurinhaController.java
+++ src/main/java/com/example/Trab_TPI/controller/FigurinhaController.java
@@ -1,6 +1,5 @@
 package com.example.Trab_TPI.controller;
 
-import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
@@ -20,19 +19,32 @@ public class FigurinhaController {
 
     @PostMapping("/gerar-figurinha")
     public String gerarFigurinha(@RequestParam("url") String url,
-                                 @RequestParam("titulo") String titulo,
-                                 @RequestParam("nomeArquivo") String nomeArquivo) throws IOException {
+            @RequestParam("titulo") String titulo,
+            @RequestParam("nomeArquivo") String nomeArquivo) throws IOException {
         BufferedImage imagemOriginal = ImageIO.read(new URL(url));
-        BufferedImage novaImagem = new BufferedImage(imagemOriginal.getWidth(), imagemOriginal.getHeight(), BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = novaImagem.createGraphics();
-        g2d.drawImage(imagemOriginal, 0, 0, null);
-        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f));
-        g2d.setColor(Color.WHITE);
-        g2d.setFont(new Font("Arial", Font.BOLD, 30));
-        g2d.drawString(titulo, 10, 40);
-        g2d.dispose();
+        int largura = imagemOriginal.getWidth();
+        int altura = imagemOriginal.getHeight();
+        int novaAltura = altura + 200;
+        BufferedImage novaImagem = new BufferedImage(largura, novaAltura,
+                BufferedImage.TYPE_INT_ARGB);
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, 0, 0, null);
 
-        File output = new File("saída/" + nomeArquivo + ".png");
+        // Configurar a fonte para escrever na imagem
+        Font fonte = new Font(Font.SANS_SERIF, Font.BOLD, 100);
+        graphics.setFont(fonte);
+        graphics.setColor(Color.YELLOW);
+
+        // Escrever uma mensagem na nova imagem
+        graphics.drawString(titulo, 200, novaAltura - 80);
+
+        String caminhoSaida = "src/main/saida/"; // Caminho relativo à pasta do projeto
+        File diretorioSaida = new File(caminhoSaida);
+        if (!diretorioSaida.exists()) {
+            diretorioSaida.mkdirs();
+        }
+
+        File output = new File(diretorioSaida, nomeArquivo + ".png");
         ImageIO.write(novaImagem, "png", output);
 
         return "redirect:/";
